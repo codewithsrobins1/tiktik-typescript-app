728f20461b8cef70fed0e478fafa1617e2a4b953
unknown <codewithsrobins1@gmail.com>
2022-08-28 16:00:30 -0700

    setup sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import GoogleLogin from 'react-google-login';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [showSideBar, setShowSideBar] = useState(true);
+
+  return (
+    <div>
+      <div
+        className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+        onClick={() => setShowSideBar((prev) => !prev)}
+      >
+        {showSideBar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
